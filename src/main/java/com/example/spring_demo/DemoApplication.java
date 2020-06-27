d1f3886eb60fed86c5800280d92043ec952f09c8
Solo <shihundusolomon@gmail.com>
2020-06-27 11:57:50 +0300

    rest controller

--- src/main/java/com/example/spring_demo/DemoApplication.java
+++ src/main/java/com/example/spring_demo/DemoApplication.java
@@ -2,11 +2,9 @@ package com.example.spring_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
 @SpringBootApplication
 public class DemoApplication {
 
@@ -14,11 +12,14 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 	
-	@RequestMapping("/")
-	@ResponseBody
-	String home()
+	@RestController
+	class DemoController
 	{
-		return "Yeeeeeeaaaahhh !!!  I'm Working !";
+		@GetMapping("/")
+		String testFunc(){
+			return "Yeeeeeeaaaahhh !!!  I'm Working !";
+		}
 	}
+	
 
 }
