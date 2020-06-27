812a3cc6004b0a5d4811502914b811accb72ca4a
Solo <shihundusolomon@gmail.com>
2020-06-27 09:50:52 +0300

    initial commit

---
+++ src/main/java/com/example/spring_demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.spring_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	@ResponseBody
+	String home()
+	{
+		return "Yeeeeeeaaaahhh !!!  I'm Working !";
+	}
+	
+
+}
