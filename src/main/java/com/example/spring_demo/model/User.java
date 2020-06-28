46e2d9f67843de7478d2f895418983c07ccbccee
Solo <shihundusolomon@gmail.com>
2020-06-28 11:09:27 +0300

    removed runner class

--- src/main/java/com/example/spring_demo/model/User.java
+++ src/main/java/com/example/spring_demo/model/User.java
@@ -5,16 +5,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "userdata")
 public class User
 {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column
+	@Column(name="id")
 	private String id;
 	
-	@Column
+	@Column(name = "name")
 	private String name;
 	
 	public User(String id,String name)
