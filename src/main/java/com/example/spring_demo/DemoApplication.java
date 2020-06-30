7734159a119809fb72bbdf99ba356c82ba0f128a
Solo <shihundusolomon@gmail.com>
2020-06-30 12:11:16 +0300

    bean config updated

--- src/main/java/com/example/spring_demo/DemoApplication.java
+++ src/main/java/com/example/spring_demo/DemoApplication.java
@@ -1,7 +1,10 @@
 package com.example.spring_demo;
 
+import com.example.spring_demo.services.UserRepository;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.spring_demo.model.User;
@@ -12,6 +15,16 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@Bean
+	ApplicationRunner applicationRunner(UserRepository userRepository)
+	{
+		return args -> {
+		userRepository.save(new User("003","Andrew Carnegi"));
+			userRepository.save(new User("004","Victor von Bistiek"));
+		};
+	}
+
 }
 
 
