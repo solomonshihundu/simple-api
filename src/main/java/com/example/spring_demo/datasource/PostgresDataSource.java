c99bf511a11c71ca1b18e9630a70a89982be25f6
Solo <shihundusolomon@gmail.com>
2020-07-03 00:44:55 +0300

    test commit

--- src/main/java/com/example/spring_demo/datasource/PostgresDataSource.java
+++ src/main/java/com/example/spring_demo/datasource/PostgresDataSource.java
@@ -18,7 +18,7 @@ public class PostgresDataSource
 		HikariConfig hikariConfig = new HikariConfig();
 
 		hikariConfig.setJdbcUrl("jdbc:hsqldb:hsql://postgres://koicciziwdtamp:e3535d3fdb967f4baedc84c13e5b10a6de42ee9f93d89b3e11e8309cf62b8192@ec2-3-208-50-226.compute-1.amazonaws.com:5432/d3pt1eea165k59");
-		hikariConfig.setDriverClassName("org.hsqldb.jdbc.JDBCDriver");
+		hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
 		hikariConfig.setUsername("koicciziwdtamp");
 		hikariConfig.setPassword("e3535d3fdb967f4baedc84c13e5b10a6de42ee9f93d89b3e11e8309cf62b8192");
 
