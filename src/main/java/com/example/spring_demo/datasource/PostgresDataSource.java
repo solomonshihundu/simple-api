052de7db99e14d3e93c6c852b4045744f875e60c
Solo <shihundusolomon@gmail.com>
2020-07-03 00:52:03 +0300

    test commit

--- src/main/java/com/example/spring_demo/datasource/PostgresDataSource.java
+++ src/main/java/com/example/spring_demo/datasource/PostgresDataSource.java
@@ -1,6 +1,5 @@
 package com.example.spring_demo.datasource;
 
-import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
@@ -14,18 +13,7 @@ public class PostgresDataSource
 	@ConfigurationProperties("app.datasource")
 	public HikariDataSource hikariDataSource()
 	{
-		HikariDataSource hikariDataSource;
-		HikariConfig hikariConfig = new HikariConfig();
-
-		hikariConfig.setJdbcUrl("jdbc:hsqldb:hsql://postgres://koicciziwdtamp:e3535d3fdb967f4baedc84c13e5b10a6de42ee9f93d89b3e11e8309cf62b8192@ec2-3-208-50-226.compute-1.amazonaws.com:5432/d3pt1eea165k59");
-		hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
-		hikariConfig.setUsername("koicciziwdtamp");
-		hikariConfig.setPassword("e3535d3fdb967f4baedc84c13e5b10a6de42ee9f93d89b3e11e8309cf62b8192");
-
-		hikariDataSource = new HikariDataSource(hikariConfig);
-		return hikariDataSource;
-
-	//	return DataSourceBuilder.create().type(HikariDataSource.class).build();
+		return DataSourceBuilder.create().type(HikariDataSource.class).build();
 	}
 
 }
